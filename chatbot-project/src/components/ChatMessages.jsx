d841b8b8d119c0889c84f7fae1ad2d424769f446
yozweb3 <zhengwenyao886@gmail.com>
2026-05-19 17:52:26 +0800

    refactor: move chatbot project to repo root

---
+++ chatbot-project/src/components/ChatMessages.jsx
@@ -0,0 +1,36 @@
+import { useRef, useEffect } from "react";
+import { ChatMessage } from "./ChatMessage";
+import "./ChatMessages.css";
+
+function useAutoScroll(dep) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const containerElem = containerRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, [dep]);
+
+  return containerRef;
+}
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useAutoScroll(chatMessages);
+
+  return (
+    <div className="chat-messages-container" ref={chatMessagesRef}>
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            isLoading={chatMessage.isLoading}
+            key={chatMessage.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
+export default ChatMessages;
