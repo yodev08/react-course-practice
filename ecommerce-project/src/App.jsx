91e13c104cd128c30610d8098816294854f9dcfd
yozweb3 <zhengwenyao886@gmail.com>
2026-05-27 15:00:08 +0800

    Move checkout page into React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/CheckoutPage";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="checkout" element={<div>test checkout working</div>} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   );
 }
