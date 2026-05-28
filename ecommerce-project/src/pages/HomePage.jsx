aa3ec72c4d2ff65c684163524a115a61b81e31d5
yozweb3 <zhengwenyao886@gmail.com>
2026-05-28 09:44:44 +0800

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,14 @@ import "./HomePage.css";
 import { products } from "../../starting-code/data/products.js";
 
 export function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <title>Ecommerce Project</title>
