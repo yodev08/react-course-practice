144707575286d9254bccb7ec1e40dd822aa565d4
yozweb3 <zhengwenyao886@gmail.com>
2026-05-28 16:41:23 +0800

    Lisf cart up and share it between pages

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,17 +3,12 @@ import { Header } from "../components/Header";
 import { useEffect, useState } from "react";
 import "./HomePage.css";
 
-export function HomePage() {
+export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     axios.get("/api/products").then((response) => {
       setProducts(response.data);
-    });
-
-    axios.get("/api/cart-items").then((response) => {
-      setCart(response.data);
     });
   }, []);
 
