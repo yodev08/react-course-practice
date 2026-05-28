06c513309248e8f01deb0843c016b389d484b250
yozweb3 <zhengwenyao886@gmail.com>
2026-05-28 10:44:07 +0800

    Use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,12 @@
+import axios from "axios";
 import { Header } from "../components/Header";
 import "./HomePage.css";
 import { products } from "../../starting-code/data/products.js";
 
 export function HomePage() {
-  fetch("http://localhost:3000/api/products")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
 
   return (
     <>
